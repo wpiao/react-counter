a7ce20ec835ce5081f27de313a4297480cbfc5ba
Wenhao Piao <wenhaopiao520@gmail.com>
2020-05-19 23:03:54 -0700

    Add Footer

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer/Footer.jsx';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Footer />
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
