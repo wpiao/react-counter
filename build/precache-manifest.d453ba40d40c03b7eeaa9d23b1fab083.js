798c04cfdab451745c093b5318d61ee153b10d93
Wenhao Piao <wenhaopiao520@gmail.com>
2020-06-07 14:36:57 -0700

    Ready for deploy

---
+++ build/precache-manifest.d453ba40d40c03b7eeaa9d23b1fab083.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c60ba2566634436c6e64e8c198b9f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25cb363fccbadde492b4",
+    "url": "/static/js/2.08631549.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08631549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4858a61e512979a926",
+    "url": "/static/js/main.1ac6df61.chunk.js"
+  },
+  {
+    "revision": "cd44af2198d2d01b28e1",
+    "url": "/static/js/runtime-main.90477c71.js"
+  }
+]);
