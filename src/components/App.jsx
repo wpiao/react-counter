d7f11c0a3b8b6a5b741a2681470fcddf0f2862aa
Wenhao Piao <wenhaopiao520@gmail.com>
2020-06-06 21:08:48 -0700

    Add Header, Footer, Counter components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+
+import Header from './Header/Header.jsx';
+import Counter from './Counter/Counter.jsx';
 import Footer from './Footer/Footer.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <Header />
+        <Counter />
         <Footer />
       </React.Fragment>
     )
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    }
+  }
+
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  decrement = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.counter}</div>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    )
+  }
+}
+
+export default Counter;
