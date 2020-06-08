21b5c04418e63d5332c5bcab4620510305f327ec
Wenhao Piao <wenhaopiao520@gmail.com>
2020-06-07 21:34:07 -0700

    Update server file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
 app.use('/', express.static(path.join(__dirname, '../build')));
 app.use(express.json());
 
-app.listen(3000, () => console.log('Server is up on port 3000!'));
+app.listen(PORT, () => console.log(`Server is up on port ${PORT}!`));
 
